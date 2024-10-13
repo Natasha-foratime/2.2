49b312c48abc7f4fa9301f85a75033adf575c905
Natasha-foratime <natasha.popova.5758@gmail.com>
2024-10-13 19:53:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const img = document.querySelector('img.carousel-item');
+
+function nextImg(){
+    img.src = 'blackcat1.jpg'
+}
+function lastImg(){
+    img.src = 'blackcat.jpg'
+}
+
+const img1 = document.querySelector('img.carousel-item1');
+function increaseImgBC(){
+    img1.src = 'blackcat.jpg'
+}
+function increaseImgBC1(){
+    img1.src = 'blackcat1.jpg'
+}
+function increaseImgOC(){
+    img1.src = 'orangecat.jpg'
+}
+function increaseImgOC1(){
+    img1.src = 'orangecat1.jpg'
+}
+function increaseImgWC(){
+    img1.src = 'whitecat.jpg'
+}
+function increaseImgWC1(){
+    img1.src = 'whitecat1.jpg'
+}
